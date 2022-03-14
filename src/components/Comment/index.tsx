0b1679ba36fe5a953abfc212bb668e1a864a6ae7
emerson-franca <emerson_franca77@hotmail.com>
2022-03-14 14:29:36 -0300

    Comments component

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,12 +1,22 @@
-import { CommentProps } from "../../@types/Comment";
-import { CommentContent, CommentDate, CommentName, Container } from "./styles";
+import { CommentType } from "../../@types/Comment";
+import {
+  CommentAvatar,
+  CommentContent,
+  CommentDate,
+  CommentInfosWrapper,
+  CommentName,
+  Container,
+} from "./styles";
 
-const Comment: React.FC<CommentProps> = ({ user, date, content }) => {
+const Comment: React.FC<CommentType> = ({ user, date, content }) => {
   return (
     <Container>
-      <CommentName>{user}</CommentName>
-      <CommentDate>{date}</CommentDate>
-      <CommentContent>{content}</CommentContent>
+      <CommentAvatar>{user.slice(0, 2).toUpperCase()}</CommentAvatar>
+      <CommentInfosWrapper>
+        <CommentName>{user}</CommentName>
+        <CommentDate>{date}</CommentDate>
+        <CommentContent>{content}</CommentContent>
+      </CommentInfosWrapper>
     </Container>
   );
 };
--- src/components/Comment/styles.ts
+++ src/components/Comment/styles.ts
@@ -1,9 +1,32 @@
 import styled from "styled-components";
 
-export const Container = styled.article``;
+export const Container = styled.article`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.mediumGrey};
+  margin-bottom: 3rem;
+  display: flex;
+  gap: 1rem;
+`;
+
+export const CommentAvatar = styled.div`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.colors.orange};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CommentInfosWrapper = styled.div``;
 
 export const CommentName = styled.p``;
 
-export const CommentDate = styled.article``;
+export const CommentDate = styled.p`
+  color: ${({ theme }) => theme.colors.lightGrey};
+  margin-bottom: 1rem;
+`;
 
-export const CommentContent = styled.p``;
+export const CommentContent = styled.p`
+  font-size: 1rem;
+  margin-bottom: 1rem;
+`;
